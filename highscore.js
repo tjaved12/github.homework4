cace31217beb2f4d1f47c1aa652ee07b62ce2b10
TJ <tahmeenaowais@yahoo.com>
2020-07-21 12:53:18 -0400

    hw fixes

--- highscore.js
+++ highscore.js
@@ -2,15 +2,21 @@ var highScore = JSON.parse(localStorage.getItem('highScore')) || [];
 // console.log('highScore', highScore);
 var ulEl = document.getElementById('list');
 var backBtn = document.getElementById('goBack');
+var clearBtn = document.getElementById('clear');
+var hscores = document.getElementById('hScores');
 // ulEl.innerHTML = "Score", ":"
 for(var i = 0; i < highScore.length; i++){
-    var liEl = document.createElement('li');
+    var liEl = document.createElement('li')
     console.log('highScore', highScore[i])
-    liEl.textContent = highScore[i].userInitial +": "+ highScore[i].userScore;
+    liEl.textContent = "User initial:"+highScore[i].userInitial+ " & "+"Score:"+highScore[i].userScore;
     ulEl.append(liEl)
 }
 
 backBtn.addEventListener("click",function(){
     console.log('open')
     window.location.href = "quiz.html";
+})
+clear.addEventListener("click",function(){
+    console.log('open');
+   ulEl.style.display="none";
 })
